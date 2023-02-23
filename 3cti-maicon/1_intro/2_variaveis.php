b8e46d385d98540e2bb6dd761488b97641a96c48
bazzukinhaa <126123321+bazzukinhaa@users.noreply.github.com>
2023-02-22 21:12:17 -0300

    Add files via upload

--- 3cti-maicon/1_intro/2_variaveis.php
+++ 3cti-maicon/1_intro/2_variaveis.php
@@ -1,11 +1,25 @@
 <?php
-	$name = "maicon";
-	$age = 20;
+	$name = "maicon"; //ler a string
 	echo $name;
-	echo "<br>";
+	echo "<hr>";
+	
+	$age = 20;  //ler o interger
 	echo $age;
 	echo "<hr>";
-	$b = true;
+	
+	$b = true;  //boolerns
 	echo gettype($b);
+	echo "<hr>";
+	
+	$nasc = 1994;  //interger
+	echo gettype($nasc);
+	echo "<hr>";
+	
+	$String = "maicon"; //string
+	echo gettype($String);
+	echo "<hr>";
+	
+	$float = 10.50; //double
+	echo gettype($float);
 	echo "<hr>";
 ?>
